adc959df7d269a784b2ea5eec6320b4403dc5838
t-whiteley <tomas.whiteley2004@gmail.com>
2023-12-11 09:17:32 +0000

    fix cost for real

--- dynamic_neurons.c
+++ dynamic_neurons.c
@@ -10,8 +10,8 @@ typedef struct Model {
     TreeNode* struct_start;
     TreeNode** inputs;
     int inputs_len;
-    TreeNode** layer1;
-    int layer1_len;
+    TreeNode** middles;
+    int middles_len;
     double eps;
     double rate;
 } Model;
@@ -43,43 +43,45 @@ int remove_dupe_nodes(TreeNode** arr, int len_arr) {
 }
 
 
-void rec_find_struct(TreeNode* root, TreeNode* prev_root, TreeNode*** arri, TreeNode*** arrl1, int* len) {
+void rec_find_struct(TreeNode* root, TreeNode*** arri, TreeNode*** arrm, int* leni, int* lenm) {
     root->data = rand_implementation(10);
     root->weight = rand_implementation(10);
     root->bias = rand_implementation(10);
 
+    if (root->id == 2) {
+        (*lenm)++;
+        *arrm = realloc(*arrm, *lenm * sizeof(TreeNode*));
+        (*arrm)[*lenm-1] = root;
+    }
+
     if (!(root->num_children)) {
-        *arri = realloc(*arri, *len * sizeof(TreeNode*));
-        (*arri)[*len-1] = root;
-        
-        *arrl1 = realloc(*arrl1, *len * sizeof(TreeNode*));
-        (*arrl1)[*len-1] = prev_root;
-        
-        (*len)++;
+        (*leni)++;
+        *arri = realloc(*arri, *leni * sizeof(TreeNode*));
+        (*arri)[*leni-1] = root;
     } else {
         for (int i = 0; i < root->num_children; i++) {
-            rec_find_struct(root->children_nodes[i], root, arri, arrl1, len);
+            rec_find_struct(root->children_nodes[i], arri, arrm, leni, lenm);
         }
     }
 }
 
 
 struct Model* init_model(TreeNode* neuron_config, double eps, double rate) {
-    int len = 0;
+    int len_i = 0;
+    int len_m = 0;
     TreeNode** inputs_arr = (TreeNode**) malloc(sizeof(TreeNode*));
-    TreeNode** layer1_arr = (TreeNode**) malloc(sizeof(TreeNode*));
-    rec_find_struct(neuron_config, NULL, &inputs_arr, &layer1_arr, &len);
-    len -= 2;
+    TreeNode** middle_arr = (TreeNode**) malloc(sizeof(TreeNode*));
+    rec_find_struct(neuron_config, &inputs_arr, &middle_arr, &len_i, &len_m);
 
-    int in_len = remove_dupe_nodes(inputs_arr, len);
-    int l1_len = remove_dupe_nodes(layer1_arr, len);
+    int in_len = remove_dupe_nodes(inputs_arr, len_i - 2);
+    int m_len = remove_dupe_nodes(middle_arr, len_m);
 
     Model* m = malloc(sizeof(Model));
     m->struct_start = neuron_config;
     m->inputs = inputs_arr;
     m->inputs_len = in_len;
-    m->layer1 = layer1_arr;
-    m->layer1_len = l1_len;
+    m->middles = middle_arr;
+    m->middles_len = m_len;
     m->eps = eps;
     m->rate = rate;
     return m;
@@ -177,8 +179,23 @@ int main() {
 
 
 
+
     double c1 = cost(m, dynamic_data, cols, rows);
-    printf("%f", c1);
+    printf("%f\n", c1);
+
+
+
+
+    print_structure(m->struct_start);
+    printf("---------\n");
+    m->middles[0]->weight -= 0.1;
+    printf("---------\n");
+
+    print_structure(m->struct_start);
+    printf("---------\n");
+
+    double c2 = cost(m, dynamic_data, cols, rows);
+    printf("%f", c2);
 
 
 
